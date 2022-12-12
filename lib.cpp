d6ce401cb16ec2fe2e16b73401578a56370a25dd
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-12 21:02:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float max(const float array[],int d) {
+    float max;
+    int i;
+    max = array[0];
+    for (i = 1; i < d; i++) {
 
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
+}
