11a125cc0ef794745cd5790ec979ea2ad43e4609
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-12 21:05:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
 int main(){
+      int x;
+ cin >> x;
+    float array[a];
+    for (int i = 0; i <= a; i++) {
+
+
+        cin >> array[i];
+    }
+
+
+    cout << max(array, x)<<endl;
+
   return 0;
 }
